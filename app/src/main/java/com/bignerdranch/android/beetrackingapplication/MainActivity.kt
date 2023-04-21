c498d88cf1c66b174a57addf33465df842da27b9
bryanferries <bryan.ferries@go.minneapolis.edu>
2023-04-20 20:20:28 -0500

    Initial Commit.  Camera Works

--- app/src/main/java/com/bignerdranch/android/beetrackingapplication/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/beetrackingapplication/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.bignerdranch.android.beetrackingapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageButton1: ImageButton
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageButton1 = findViewById(R.id.imageButton1)
+        imageButton1.setOnClickListener {
+            takePicture()
+        }
+
+    }
+
+    private fun takePicture() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivity(takePictureIntent)
     }
 }
